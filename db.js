1d0363be20b16071162e4364288208744455a3ab
farizirianov <farialis92@gmail.com>
2020-07-02 10:19:40 -0400

    commit

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const moongose = require('mongoose')
 const env = require('./config/env.js')
 require('dotenv').config()
 
-const url = `mongodb+srv://${env.db_user}:${env.db_password}@${env.db_link}.mongodb.net/${env.db_name}?retryWrites=true&w=majority`
+const url = `mongodb://localhost:27017/apigrun`
 moongose
     .connect(url,{
         useNewUrlParser: true,
