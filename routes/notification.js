cb2074bb0a2a6a0b8d6cc729739b528d2a76ff29
farizirianov <farialis92@gmail.com>
2020-07-05 17:11:25 -0400

    notifi

--- routes/notification.js
+++ routes/notification.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
 const cNotificacion = require('../controllers/notification')
 
+router.post('/user/:id', cNotificacion.createNotification)
 module.exports = router
